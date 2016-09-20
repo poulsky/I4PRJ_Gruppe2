b6292f5a5770d00129467f4d7f8d98149e630f44
SimonThrane <minijordan23-virum@hotmail.com>
2016-09-20 16:47:58 +0200

    Oprettelse af ASP.NET projekt

---
+++ Source code/LocaTile/LocaTile/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LocaTile.MvcApplication" Language="C#" %>
---
+++ Source code/LocaTile/LocaTile/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocaTile.Startup))]
+namespace LocaTile
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
