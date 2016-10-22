7e7b446d6ac3ec8b1367b127f4066292eb300384
Lars <hjerrildlars@gmail.com>
2016-10-22 17:34:04 +0200

    Udvidelse af Seed metode

--- Source code v2/BargainBarterV2/BargainBarterV2/Migrations/Configuration.cs
+++ Source code v2/BargainBarterV2/BargainBarterV2/Migrations/Configuration.cs
@@ -6,6 +6,7 @@ namespace BargainBarterV2.Migrations
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.IO;
     using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<BargainBarterV2.Models.ApplicationDbContext>
@@ -15,11 +16,21 @@ namespace BargainBarterV2.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
+        private byte[] Readpicture(string filename)
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(filename);
+
+            return bytes;
+        }
+
         protected override void Seed(BargainBarterV2.Models.ApplicationDbContext context)
         {
 
             var passwordHash = new PasswordHasher();
             string password = passwordHash.HashPassword("Password@123");
+
+            #region SeedingUsers
+
             context.Users.AddOrUpdate(u => u.UserName,
                 new ApplicationUser
                 {
@@ -37,10 +48,226 @@ namespace BargainBarterV2.Migrations
                         StreetName = "Jensbaggesensvej",
                         City = "Aarhus",
                         PostalCode = "8200",
-                        StreetNumber = "13",
+                        StreetNumber = "13"
                     }
 
                 });
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+                    UserName = "Jens@go.com",
+                    PasswordHash = password,
+                    PhoneNumber = "15150882",
+                    PhoneNumberConfirmed = true,
+                    Email = "Jens@go.com",
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    Firstname = "Jens",
+                    Lastname = "Didriksen",
+                    Address = new Address()
+                    {
+                        StreetName = "Bredgade",
+                        City = "Tilst",
+                        PostalCode = "4200",
+                        StreetNumber = "111"
+                    }
+                });
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+                    UserName = "Jakob@go.com",
+                    PasswordHash = password,
+                    PhoneNumber = "15150883",
+                    PhoneNumberConfirmed = true,
+                    Email = "Jakob@go.com",
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    Firstname = "Jakob",
+                    Lastname = "Andersen",
+                    Address = new Address()
+                    {
+                        StreetName = "Nygade",
+                        City = "Aalborg",
+                        PostalCode = "8410",
+                        StreetNumber = "7"
+                    }
+                });
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+                    UserName = "Søren@go.com",
+                    PasswordHash = password,
+                    PhoneNumber = "15150884",
+                    PhoneNumberConfirmed = true,
+                    Email = "Søren@go.com",
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    Firstname = "Søren",
+                    Lastname = "Holm",
+                    Address = new Address()
+                    {
+                        StreetName = "Berstoftvej",
+                        City = "Malling",
+                        PostalCode = "8290",
+                        StreetNumber = "265"
+                    }
+                });
+
+            #endregion
+
+            #region SeedingAds
+
+            context.BarterAdds.AddOrUpdate(add => add.CreatedDateTime, new BarterAdd
+            {
+                Titel = "Boremaskine",
+                Description = "Bosch reklamerer selv med at den er til gør-det-selv manden," +
+                              " men du skal vide at du også går på kompromis med både kvalitet" +
+                              " og holdbarhed. Jeg har selv haft flere grønne fra Bosch og kan" +
+                              " konstatere at pris og kvalitet hænger sammen. Eksempelvis har jeg" +
+                              " nogle gange oplevet at aklsen er blevet skæv, og så er det umuligt" +
+                              " at bore et lige hul.",            
+                Category = "Elektronik",
+                CreatedDateTime = new DateTime(2016, 8, 4, 12, 30, 45),
+
+                Picture = Readpicture(@"BargainBarterV2\Content\img\SeedingAdsPictures\bosch-gron-boremaskine.png"),
+                ApplicationUser = context.Users.First()
+
+        });
+
+            context.BarterAdds.AddOrUpdate(add => add.CreatedDateTime, new BarterAdd
+            {
+                Titel = "Arduino nano",
+                Description = "The Arduino Nano is a small, complete, and breadboard-friendly" +
+                              " board based on the ATmega328 (Arduino Nano 3.x) or ATmega168 " +
+                              "(Arduino Nano 2.x). It has more or less the same functionality of the " +
+                              "Arduino Duemilanove, but in a different package. It lacks only a" +
+                              " DC power jack, and works with a Mini-B USB cable instead of a " +
+                              "standard one. The Nano was designed and is being produced by Gravitech.",
+                Category = "Elektronik",
+                CreatedDateTime = new DateTime(2016, 8, 9, 12, 30, 45),
+
+                Picture = Readpicture(@"BargainBarterV2\Content\img\SeedingAdsPictures\ArduinoNanoFront.jpg"),
+                
+            });
+
+            context.BarterAdds.AddOrUpdate(add => add.CreatedDateTime, new BarterAdd
+            {
+                Titel = "Arduino Mega",
+                Description = "The Mega 2560 is a microcontroller board based on the ATmega2560." +
+                              " It has 54 digital input/output pins (of which 15 can be used as PWM " +
+                              "outputs), 16 analog inputs, 4 UARTs (hardware serial ports), a 16 MHz " +
+                              "crystal oscillator, a USB connection, a power jack, an ICSP header, and " +
+                              "a reset button. It contains everything needed to support the " +
+                              "microcontroller; simply connect it to a computer with a USB cable or" +
+                              " power it with a AC-to-DC adapter or battery to get started. The Mega " +
+                              "2560 board is compatible with most shields designed for the Uno and the " +
+                              "former boards Duemilanove or Diecimila.",
+                Category = "Elektronik",
+                CreatedDateTime = new DateTime(2016, 8, 11, 12, 30, 45),
+
+                Picture = Readpicture(@"BargainBarterV2\Content\img\SeedingAdsPictures\ArduinoMega.jpg"),
+
+            });
+
+            context.BarterAdds.AddOrUpdate(add => add.CreatedDateTime, new BarterAdd
+            {
+                Titel = "T-Shirt",
+                Description = "Lækker hvid trøje fra H&M som er i helt vildt god stand," +
+                              " men som er blevet for lille",
+                Category = "Tøj",
+                CreatedDateTime = new DateTime(2016, 9, 11, 12, 30, 45),
+
+                Picture = Readpicture(@"BargainBarterV2\Content\img\SeedingAdsPictures\t-shirt.jpg"),
+
+            });
+
+            context.BarterAdds.AddOrUpdate(add => add.CreatedDateTime, new BarterAdd
+            {
+                Titel = "Sweatshirt",
+                Description = "Det er en sweatshirt i ret tøsede farver størrelsen er large" +
+                              ", Den er lettere brugt og har et par huller",
+                Category = "Tøj",
+                CreatedDateTime = new DateTime(2016, 10, 11, 12, 30, 45),
+
+                Picture = Readpicture(@"BargainBarterV2\Content\img\SeedingAdsPictures\sweatshirt.jpg"),
+
+            });
+
+            context.BarterAdds.AddOrUpdate(add => add.CreatedDateTime, new BarterAdd
+            {
+                Titel = "Jeans",
+                Description = "Usle dårlige jeans, som bare ikke er ret pæne, men de kan måske" +
+                              "ordnes ",
+                Category = "Tøj",
+                CreatedDateTime = new DateTime(2016, 10, 12, 12, 30, 45),
+
+                Picture = Readpicture(@"BargainBarterV2\Content\img\SeedingAdsPictures\jeans.jpg"),
+
+            });
+
+            context.BarterAdds.AddOrUpdate(add => add.CreatedDateTime, new BarterAdd
+            {
+                Titel = "Hættetrøje",
+                Description = " bluse med lange ærmer og rund hals, fremstillet i kraftig " +
+                              "bomuld med en lodden vrangside, der bæres ind mod kroppen." +
+                              " Sweatshirten er blevet brugt af sportsfolk gennem hele 1900-t.; " +
+                              "fra 1960'erne blev den en del af den almindelige påklædning, og man" +
+                              " begyndte at påtrykke navne, logoer, mønstre og billeder.",
+                Category = "Tøj",
+                CreatedDateTime = new DateTime(2016, 11, 12, 12, 30, 45),
+
+                Picture = Readpicture(@"BargainBarterV2\Content\img\SeedingAdsPictures\hættetrøje.jpg"),
+
+            });
+
+
+            context.BarterAdds.AddOrUpdate(add => add.CreatedDateTime, new BarterAdd
+            {
+                Titel = "Klaverstol",
+                Description = "En standard klaverstol, den kan justeres i højden og er rigtig god. " +
+                              "Træet er lidt mørt men stolen har mange år i sig endnu",
+                Category = "Interiør",
+                CreatedDateTime = new DateTime(2016, 02, 01, 12, 30, 45),
+
+                Picture = Readpicture(@"BargainBarterV2\Content\img\SeedingAdsPictures\klaverstol.jpg"),
+
+            });
+
+            context.BarterAdds.AddOrUpdate(add => add.CreatedDateTime, new BarterAdd
+            {
+                Titel = "Spisebord",
+                Description = "Et bord er et møbel bestående af en vandret flade af varierende materiale, " +
+                              "som er hævet fra gulvhøjde, typisk vha. fire bordben. Bordet har det" +
+                              " formål at man kan stille ting fra sig, eller at man kan sidde omkring" +
+                              " det som et samlingssted, eller endda en kombination af begge. Der findes" +
+                              " mange forskellige slags design af borde.",
+                Category = "Interiør",
+                CreatedDateTime = new DateTime(2016, 02, 03, 12, 29, 45),
+
+                Picture = Readpicture(@"BargainBarterV2\Content\img\SeedingAdsPictures\spisebord.jpg"),
+
+            });
+
+            context.BarterAdds.AddOrUpdate(add => add.CreatedDateTime, new BarterAdd
+            {
+                Titel = "To mands Sofa",
+                Description = "Min sofa er der som sådan ikke noget galt med, den er jeg vildt glad " +
+                              "for, både i design og funktion. Det var DEN sofa jeg ville have også " +
+                              "selvom den sprang vores daværende budget med 10.000 kr. Det er farven" +
+                              " den er gal med. Jeg går altid efter en grå sofa, grå er en lækker farve" +
+                              " og den passer til ALT. Med en grå sofa kan jeg skifte stil så tosset" +
+                              " jeg vil, og den vil altid passe ind. ",
+                Category = "Interiør",
+                CreatedDateTime = new DateTime(2016, 04, 03, 12, 29, 45),
+
+                Picture = Readpicture(@"BargainBarterV2\Content\img\SeedingAdsPictures\sofa.jpg"),
+
+                
+
+            });
+
+            #endregion
+
         }
     }
 }
